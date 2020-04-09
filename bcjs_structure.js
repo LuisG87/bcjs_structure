66a0ef710205974e839c41908400398c38b1e7d8
Luis Gomez <luisg2249@gmail.com>
2020-04-09 13:52:22 -0300

    alpha ver 1.01

--- bcjs_structure.js
+++ bcjs_structure.js
@@ -4,88 +4,130 @@
  */
 
 //example "div>div,div(class:a e,id:an id)[atext]>p"
-bcjs_structure = function(structure){
-  
-  /**
-   * @function asocciateElements
-   * @description
-   * @param {objects array} tags 
-   */
-  function associateElements(tags){
-    if (tags.length>1){
-      i = 0;
-      do{
-        tags[i].appendChild(tags[i+1]);
-        i++;
-      }while(i<tags.length-1);
-    }
-  }
-
-
-  /**
-   * @function addAttributes
-   * @description pass a text "element" and an element "tag"
-   * @param {string} str like div(class:a)
-   * @param {object element html} tag 
-   */
-  function addAttributes(str,tag){
-    var atributos = str.substring(str.indexOf("(") + 1, str.lastIndexOf(")"));
-    data = [];
-    if(atributos.includes(",")){
-      atrList = atributos.split(",");
-      
-      for(i in atrList){
-        atr = atrList[i].split(":");
-        data[i] = [atr[0],atr[1]];
+bcjs_strucutre = function(){
+  this.tags =[];
+  this.structure = function(structure){
+    this.tags =[];
+    /**
+     * @function asocciateElements
+     * @description
+     * @param {objects array} tags 
+     */
+    this.associateElements = function(tags){
+      if (tags.length>1){
+        i = 0;
+        do{
+          tags[i].appendChild(tags[i+1]);
+          i++;
+        }while(i<tags.length-1);
       }
+    }
+
+
+    /**
+     * @function addAttributes
+     * @description pass a text "element" and an element "tag"
+     * @param {string} str like div(class:a)
+     * @param {object element html} tag 
+     */
+    this.addAttributes= function(text,tag){
+      closeParentesisIndex = text.lastIndexOf(")");
+      text = text.substring(0, closeParentesisIndex);
+      data = [];
       
-    }else{
-      atr = atributos.split(":");
-      data[0] =[atr[0],atr[1]];
-    }
-    for(d in data){
-      tag.setAttribute(data[d][0],data[d][1]);
-    }
-  }
-
-  /**
-   * @function addText
-   * @description Add a text to an html object determined by []
-   * @param {string} str 
-   * @param {Object HTML} tag 
-   */
-  function addText(str,tag){
-    if(str.includes("[")){
-      var text = str.substring(str.lastIndexOf("[") + 1, str.lastIndexOf("]"));
-      tag.textContent = text;
-    }
-  }
-
-
-
-  var tags = [];
-  structure = structure.split(">");
-  //create structure with atributes
-  for(i in structure){
-
-      if(structure[i].includes("(")){
-
-        aux = structure[i].split("(");
-        tags[i] = document.createElement(aux[0]);
-
-        addAttributes(structure[i],tags[i]);
-      }else if(structure[i].includes("[")){
-        aux = structure[i].split("[");
-        tags[i] = document.createElement(aux[0]);
+      if(text.includes(",")){
+        atrList = text.split(",");
+        
+        for(i in atrList){
+          atr = atrList[i].split(":");
+          data[i] = [atr[0],atr[1]];
+        }
+        
       }else{
-        tags[i] = document.createElement(structure[i]);
+        atr = text.split(":");
+        data[0] =[atr[0],atr[1]];
       }
+      for(d in data){
+        tag.setAttribute(data[d][0],data[d][1]);
+      }
+    }
+
+    /**
+     * @function addText
+     * @description Add a text to an html object determined by []
+     * @param {string} str 
+     * @param {Object HTML} tag 
+     */
+    this.addText= function(text,tag){
+      if(text.includes("[")){
+        var text = text.substring(text.lastIndexOf("[") +1, text.lastIndexOf("]"));
+        tag.textContent = text;
+      }
+    }
+
+    this.getElement = function(structure,i){
+      var s;
       
-      addText(structure[i],tags[i]);
+      if(i== "none"){
+        s = structure;
+        i = 0;
+      }else{
+        s = structure[i];
+      }
 
+      if(s.includes("(")){
+
+        parentesisIndex = s.indexOf("(");
+        newElement = s.substring(0,parentesisIndex);
+        this.tags[i] = document.createElement(newElement);
+
+        subtext = s.substring(parentesisIndex+1,s.length);
+        this.addAttributes(subtext,this.tags[i]);
+
+      }else if(s.includes("[")){
+        aux = s.split("[");
+        this.tags[i] = document.createElement(aux[0]);
+        
+      }else{
+        this.tags[i] = document.createElement(s);
+      }
+      this.addText(s,this.tags[i]);
+    }
+
+
+
+
+    //var tags = [];
+    if(structure.includes(">")){
+      structure = structure.split(">");
+      //create structure with atributes
+      for(i in structure){
+
+        this.getElement(structure,i);
+
+          /*if(structure[i].includes("(")){
+
+            aux = structure[i].split("(");
+            tags[i] = document.createElement(aux[0]);
+
+            this.addAttributes(structure[i],tags[i]);
+          }else if(structure[i].includes("[")){
+            aux = structure[i].split("[");
+            tags[i] = document.createElement(aux[0]);
+            
+          }else{
+            tags[i] = document.createElement(structure[i]);
+          }
+          this.addText(structure[i],tags[i]);*/
+          this.associateElements(this.tags);
+      }
+    }else{
+      this.getElement(structure,"none");
+    }
+    /*asociate elements*/
+    
+    return this.tags[0];
   }
-  /*asociate elements*/
-  associateElements(tags);
-  return tags[0];
-}
+};
 
+bcjs = new bcjs_strucutre();
